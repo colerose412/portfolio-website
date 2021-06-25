793ffae7b53b7218ead9e93c553325b04f59a218
colerose412 <colerose412@gmail.com>
2021-06-25 12:04:15 -0400

    Added toggle menu
    Edited all files to create the toggle menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log('Hello world');
+
+//make a responsive menu
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
